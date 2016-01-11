28f9ed19e3f2f8a36398f6ece69359da11dd92a3
James Kirk <jimeux@gmail.com>
2016-01-11 20:20:44 +0900

    Complete 3-card animation cycle.

--- app/src/main/java/com/moobasoft/yezna/ui/activities/MainActivity.java
+++ app/src/main/java/com/moobasoft/yezna/ui/activities/MainActivity.java
@@ -4,10 +4,15 @@ import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.moobasoft.yezna.R;
@@ -26,6 +31,7 @@ public class MainActivity extends BaseActivity implements SummaryClickListener {
 
     @Bind(R.id.container) ViewGroup container;
     @Bind(R.id.toolbar)   Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle state) {
@@ -36,6 +42,63 @@ public class MainActivity extends BaseActivity implements SummaryClickListener {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
         fragmentManager = getFragmentManager();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
+        view.setNavigationItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+                /** Auth-related items */
+                case R.id.action_login:
+                    Intent loginIntent = new Intent(this, ConnectActivity.class);
+                    loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    loginIntent.putExtra(ConnectActivity.REGISTER, false);
+                    startActivity(loginIntent);
+                    break;
+
+                case R.id.action_register:
+                    Intent registerIntent = new Intent(this, ConnectActivity.class);
+                    registerIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    registerIntent.putExtra(ConnectActivity.REGISTER, true);
+                    startActivity(registerIntent);
+                    break;
+
+                case R.id.action_logout:
+                    credentialStore.delete();
+                    Snackbar.make(container, getString(R.string.logout_success), Snackbar.LENGTH_SHORT).show();
+                    break;
+            }
+            Snackbar.make(container, item.getTitle() + " pressed", Snackbar.LENGTH_LONG).show();
+            item.setChecked(true);
+            drawerLayout.closeDrawers();
+            return true;
+        });
+
+
+        // Initializing Drawer Layout and ActionBarToggle
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.login, R.string.logout){
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessay or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+
+
 
         if (state == null)
             fragmentManager.beginTransaction()
@@ -52,15 +115,19 @@ public class MainActivity extends BaseActivity implements SummaryClickListener {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         boolean loggedIn = credentialStore.isLoggedIn();
-        menu.findItem(R.id.action_logout).setVisible(loggedIn);
+        /*menu.findItem(R.id.action_logout).setVisible(loggedIn);
         menu.findItem(R.id.action_login).setVisible(!loggedIn);
-        menu.findItem(R.id.action_register).setVisible(!loggedIn);
+        menu.findItem(R.id.action_register).setVisible(!loggedIn);*/
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
 
             case R.id.action_create:
                 promptForLogin(toolbar);
--- app/src/main/java/com/moobasoft/yezna/ui/fragments/RandomFragment.java
+++ app/src/main/java/com/moobasoft/yezna/ui/fragments/RandomFragment.java
@@ -29,25 +29,38 @@ import static android.view.View.VISIBLE;
 
 public class RandomFragment extends RxFragment implements RandomPresenter.View, QuestionView.QuestionClickListener {
 
-    /** Bundle keys for saving/restoring instance state. */
-    public static final String PAGE_KEY      = "page_key";
+    /**
+     * Bundle keys for saving/restoring instance state.
+     */
+    public static final String PAGE_KEY = "page_key";
     public static final String QUESTIONS_KEY = "summaries_key";
 
-    /** The number of questions returned per page. */
+    /**
+     * The number of questions returned per page.
+     */
     public static final int PER_PAGE = 10;
 
-    /** The current page of question data. Used as a query param. */
+    /**
+     * The current page of question data. Used as a query param.
+     */
     private int currentPage = 1;
 
-    /** A data structure to store question instances. */
+    /**
+     * A data structure to store question instances.
+     */
     private ArrayList<Question> questions;
 
-    /** A container ViewGroup for the question card views. */
-    @Bind(R.id.content) ViewGroup cardDeck;
+    /**
+     * A container ViewGroup for the question card views.
+     */
+    @Bind(R.id.content)
+    ViewGroup cardDeck;
 
-    @Inject RandomPresenter presenter;
+    @Inject
+    RandomPresenter presenter;
 
-    public RandomFragment() {}
+    public RandomFragment() {
+    }
 
     @Override
     public void onCreate(@Nullable Bundle state) {
@@ -62,7 +75,8 @@ public class RandomFragment extends RxFragment implements RandomPresenter.View, 
         }
     }
 
-    @Nullable @Override
+    @Nullable
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state) {
         View view = inflater.inflate(R.layout.fragment_question, container, false);
         ButterKnife.bind(this, view);
@@ -110,15 +124,8 @@ public class RandomFragment extends RxFragment implements RandomPresenter.View, 
         else {
             activateContentView();
             this.questions.addAll(questions);
-
-            if (currentPage == 1) {
-                if (questions.size() > 2)
-                    pushCard(questions.get(2), 1F, -2F);
-                if (questions.size() > 1)
-                    pushCard(questions.get(1), 2F, -1F);
-                if (questions.size() > 0)
-                    pushCard(questions.get(0), 3F, 0F);
-            }
+            if (currentPage == 1)
+                initDeck();
         }
         currentPage++;
     }
@@ -137,75 +144,73 @@ public class RandomFragment extends RxFragment implements RandomPresenter.View, 
     }
 
     @Override
-    public void onAnswerQuestion(Question question, final View view, boolean yes) {
+    public void onAnswerQuestion(Question question, View view, boolean yes) {
         //if (questions.size() == Math.ceil(PER_PAGE / 2))
-          //  loadPosts(false);
-
+        //  loadPosts(false);
         presenter.answerQuestion(question.getId(), yes);
+        animateCardRemoval(view);
+    }
 
-        int angle = yes ? 45 : -45;
-        int deltaX = yes ? 2000 : -1400;
-        int deltaY = yes ? -1800 : -1100;
-
-        Animation rotate = new RotateAnimation(0, angle);
-        Animation translate = new TranslateAnimation(0, deltaX, 0, deltaY);
+    private void animateCardRemoval(final View view) {
+        Animation rotate = new RotateAnimation(0, 45);
+        Animation translate = new TranslateAnimation(0, 1800, 0, -1900);
 
         AnimationSet set = new AnimationSet(true);
         set.addAnimation(rotate);
         set.addAnimation(translate);
-        set.setDuration(350);
-        set.setFillAfter(true);
+        set.setDuration(450);
         set.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
-                for (int i = 0; i < cardDeck.getChildCount(); i++)
-                    cardDeck.getChildAt(i).setLayerType(View.LAYER_TYPE_HARDWARE, null);
+                setCardHardwareAcceleration(true);
 
                 questions.remove(0);
                 ViewCompat.setTranslationZ(view, 4F);
-                cardDeck.removeView(view);
+                cardDeck.removeViewAt(0);
 
-                if (questions.size() == 2)
-                    pushCard(questions.get(1), 2F, -1F);
-                else if (questions.size() > 2)
-                    pushCard(questions.get(2), 1F, -2F);
+                if (questions.size() > 2)
+                    pushCard(questions.get(2));
 
-                if (cardDeck.getChildCount() >= 1) {
-                    View topCard = cardDeck.getChildAt(cardDeck.getChildCount() - 1);
-                    ViewCompat.setTranslationZ(topCard, 3F);
-                    topCard.animate().rotation(0F).start();
-                }
-                if (cardDeck.getChildCount() >= 2) {
-                    View secondCard = cardDeck.getChildAt(cardDeck.getChildCount() - 2);
-                    ViewCompat.setTranslationZ(secondCard, 2F);
-                    secondCard.animate().rotation(-1F).start();
-                }
-                if (cardDeck.getChildCount() >= 3) {
-                    View thirdCard = cardDeck.getChildAt(cardDeck.getChildCount() - 3);
-                    ViewCompat.setTranslationZ(thirdCard, 1F);
-                    thirdCard.animate().rotation(-2F).start();
-                }
+                adjustCards();
             }
-            @Override public void onAnimationEnd(Animation animation) {
-                view.setLayerType(View.LAYER_TYPE_NONE, null);
-                for (int i = 0; i < cardDeck.getChildCount(); i++)
-                    cardDeck.getChildAt(i).setLayerType(View.LAYER_TYPE_NONE, null);
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                setCardHardwareAcceleration(false);
             }
-            @Override public void onAnimationRepeat(Animation animation) {}
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
         });
-
-        view.setLayerType(View.LAYER_TYPE_HARDWARE, null);
         view.startAnimation(set);
     }
 
-    private void pushCard(Question question, float zIndex, float rotation) {
-        QuestionView view = (QuestionView) LayoutInflater.from(getActivity())
+    private void initDeck() {
+        for (int i = 0; i < 3 && i < questions.size(); i++)
+            pushCard(questions.get(i));
+        adjustCards();
+    }
+
+    private void adjustCards() {
+        for (int position = 0; position < cardDeck.getChildCount(); position++) {
+            View card = cardDeck.getChildAt(position);
+            ViewCompat.setTranslationZ(card, 4.0F - (position + 1));
+            card.animate().setDuration(220).rotation(1.0F - (position + 1)).start();
+        }
+    }
+
+    private void pushCard(Question question) {
+        QuestionView card = (QuestionView) LayoutInflater.from(getActivity())
                 .inflate(R.layout.view_question, cardDeck, false);
-        view.bindTo(question, this);
+        card.bindTo(question, this);
+        card.setRotation(-2F);
+        cardDeck.addView(card, cardDeck.getChildCount());
+    }
 
-        view.setRotation(rotation);
-
-        cardDeck.addView(view, 0);
-        ViewCompat.setTranslationZ(cardDeck.getChildAt(0), zIndex);
+    private void setCardHardwareAcceleration(boolean activate) {
+        int layerType = activate ? View.LAYER_TYPE_HARDWARE : View.LAYER_TYPE_NONE;
+        for (int i = 0; i < cardDeck.getChildCount(); i++)
+            cardDeck.getChildAt(i).setLayerType(layerType, null);
     }
 }
